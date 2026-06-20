7c8f51640574ceba5ead3dc77650a2dae42aaca6
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-06-20 18:22:01 +0200

    We do not want duplicate ValidBetween (same id + version)

--- .github/scripts/validate-should-fail.sh
+++ .github/scripts/validate-should-fail.sh
@@ -34,5 +34,6 @@ assert_rejected() { # <file> <schema> <expected error substring>
 echo "Checking NeTEx 'should-fail' negative examples ..."
 assert_rejected examples/should-fail/duplicate-GroupOfLinkSequences.xml xsd/NeTEx_publication.xsd "Duplicate key-sequence"
 assert_rejected examples/should-fail/duplicate-CalendarDate.xml         xsd/NeTEx_publication.xsd "Duplicate key-sequence"
+assert_rejected examples/should-fail/duplicate-ValidBetween.xml         xsd/NeTEx_publication.xsd "Duplicate key-sequence"
 
 exit "${fail}"
